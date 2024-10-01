19a7cb48c3202e96b4e55224909534255bd486d5
jordy Eulink <jordy.eulink31@gmail.com>
2024-10-01 10:07:27 +0200

    t/m stap 3

--- src/main/java/han/aim/se/noyoumaynot/movie/controller/MovieController.java
+++ src/main/java/han/aim/se/noyoumaynot/movie/controller/MovieController.java
@@ -14,6 +14,9 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/movies")
 public class MovieController {
+
+    private final String username = "test";
+    private final String password ="123";
     private final MovieService movieService;
     private final AuthenticationService authenticationService;
 
@@ -29,20 +32,22 @@ public class MovieController {
     }
 
     @GetMapping("/show")
-    public Movie getMovieById(@RequestParam("id") String id) {
-
+    public Movie getMovieById(@RequestParam("id") String id) throws Exception {
+            authenticate("");
             Movie movie = movieService.getMovieById(id);
             return movie;
     }
 
     @PostMapping("/add")
-    public Movie addMovie(@RequestBody Movie movie) {
+    public Movie addMovie(@RequestBody Movie movie) throws Exception {
+        authenticate("");
         movieService.insertMovie(movie);
         return movie;
     }
 
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<String> deleteMovie(@PathVariable("id") String id) {
+    public ResponseEntity<String> deleteMovie(@PathVariable("id") String id) throws Exception {
+        authenticate("");
         movieService.deleteMovie(id);
         return ResponseEntity.ok().build();
     }
---
+++ src/main/java/han/aim/se/noyoumaynot/movie/exceptionhandler/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package han.aim.se.noyoumaynot.movie.exceptionhandler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("An error occurred: " + ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
